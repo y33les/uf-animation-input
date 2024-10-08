8f5f1db781e913da1e63b1c4f646469182d7c044
Phil Yeeles <phil@yeel.es>
2024-10-08 16:08:05 +0100

    animated sprite working; blank-sprite added; .gitignore added

--- anim.f
+++ anim.f
@@ -8,11 +8,29 @@ h# 0fff b ! \ CGA colours
 r @ g @ b @ colors
 
 \ square sprite
-variable sq 4 cells allot
+variable sq 16 cells allot
+variable sqframe 1 allot \ sprite current frame number
+\ frame 0
 h# ff81 sq !
 h# 8181 sq 1 cells + !
 h# 8181 sq 2 cells + !
 h# 81ff sq 3 cells + !
+\ frame 1
+sq 1 cells + @ sq 4 cells + !
+sq 2 cells + @ sq 5 cells + !
+sq 3 cells + @ sq 6 cells + !
+sq @ sq 7 cells + !
+\ frame 2
+sq 2 cells + @ sq 8 cells + !
+sq 3 cells + @ sq 9 cells + !
+sq @ sq 10 cells + !
+sq 1 cells + @ sq 11 cells + !
+\ frame 3
+sq 3 cells + @ sq 12 cells + !
+sq @ sq 13 cells + !
+sq 1 cells + @ sq 14 cells + !
+sq 2 cells + @ sq 15 cells + !
+
 sq spritedata
 
 \ position management
@@ -29,7 +47,7 @@ variable es 4 cells allot
 0 es 2 cells + !
 0 es 3 cells + !
 
-\ move sprite on button press
+\ move sprite on button press TODO: smooth motion
 : move-it ( -- )
     0 \ jvector seems to expect some kind of input?
     jbutton
@@ -51,18 +69,27 @@ variable es 4 cells allot
       drop
     then ;
 
+\ draw blank sprite at current position
+: blank-sprite ( -- )
+    es spritedata \ blank sprite FIXME: overwriting with an empty sprite doesn't really feel like the best way to do this
+    h# 41 sprite ;
+
+\ draw square sprite given frame number
+: animate-square ( -- )
+    sqframe c@ 4 /mod nip cells sq + spritedata
+    h# 41 sprite ;
+
 \ animate colours
 : cycle-colours ( -- )
-    es spritedata \ FIXME: overwriting with an empty sprite doesn't really feel like the best way to do this
-    h# 41 sprite
-    sq spritedata
     0 \ svector seems to expect some kind of input?
     r @ h# 111 + r !
     g @ h# 222 + g !
     b @ h# 333 + b !
     r @ g @ b @ colors
+    blank-sprite
     x @ y @ position
-    h# 41 sprite ;
+    sqframe c@ 1+ 16 /mod drop sqframe c!
+    animate-square ;
 
 ' move-it jvector
 ' cycle-colours svector
